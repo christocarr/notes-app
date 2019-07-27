d2c1a94e7e49bcb25d18cafa3c64c88c31e91f9d
Chris Carr <christocarr@outlook.com>
2019-07-27 16:58:50 +0100

    Add functionality to edit and delete notes

---
+++ notes-edit.js
@@ -0,0 +1,30 @@
+const noteTitle = document.getElementById('noteTitle');
+const noteBody = document.getElementById('noteBody');
+const removeNote = document.getElementById('removeNote');
+const noteId = location.hash.substring(1);
+const notes = getSavedNotes();
+const note = notes.find(function(note) {
+  return note.id === noteId;
+});
+
+if(note === undefined) {
+  location.assign('/index.html');
+}
+noteTitle.value = note.title;
+noteBody.textContent = note.body;
+
+//save note title and body when edited
+noteTitle.addEventListener('keyup', function(e) {
+  note.title = e.target.value;
+  saveNotes(notes);
+});
+noteBody.addEventListener('keyup', function(e) {
+  note.body = e.target.value;
+  saveNotes(notes);
+});
+
+removeNote.addEventListener('click', function() {
+  deleteNote(note.id);
+  saveNotes(notes);
+  location.assign('/index.html');
+})
